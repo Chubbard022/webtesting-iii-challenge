233abbcb68e846353f5547cb945cdaa9bf2277bb
Chubbard022 <chubbard022@gmail.com>
2019-05-15 12:24:56 -0700

    first test passing for display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from "react"
+import {render,cleanup,fireEvent,getByTestId} from "react-testing-library"
+import Display from "./Display"
+
+describe("<Display/>",()=>{
+    it("should display unlocked and Open without buttons pressed" ,()=>{
+        const display_component = render(<Display />)
+
+        display_component.getByText(/unlocked/i)
+        display_component.getByText(/open/i)
+    })
+})
