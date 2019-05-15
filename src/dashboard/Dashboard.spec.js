9a6f4e70f653621363f5562cb7b277bbb790c818
Chubbard022 <chubbard022@gmail.com>
2019-05-15 12:10:33 -0700

    dashboard testing working to make sure components render

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
-// Test away
+import React from "react"
+import {render,cleanup,fireEvent,getByTestId} from "react-testing-library"
+import Dashboard from "./Dashboard"
+
+describe("<Dashboard/>",()=>{
+    it("should contain <Display/> and <Controls/> component",()=>{
+        const dash_component = render(<Dashboard/>)
+
+        dash_component.getByText(/open/i)
+        dash_component.getByText(/unlocked/i)
+    })
+})
