8114a50383cc063c90ea3693e79d483883965183
Chubbard022 <chubbard022@gmail.com>
2019-05-15 12:50:49 -0700

    first test for controls component working:

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,17 @@
-// Test away!
+import React from "react"
+import {render,cleanup,fireEvent,getByTestId} from "react-testing-library"
+import Display from "../display/Display"
+import Controls from "../controls/Controls"
+import Dashboard from "../dashboard/Dashboard"
+
+
+describe("<Controls/>",()=>{
+    it("should show unlocked after button pressed",()=>{
+        const control_component = render(<Controls/>)
+        const display_component = render(<Display />)
+        const button = control_component.getByText(/lock gate/i)
+
+        fireEvent.click(button)
+        display_component.getByText(/unlocked/i)
+    })
+})
