de57af86e73a599f04964e350b79e3b10275c8d9
Chubbard022 <chubbard022@gmail.com>
2019-05-15 12:33:24 -0700

    testing to see if button changes displayed text working

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,8 @@
 import React from "react"
 import {render,cleanup,fireEvent,getByTestId} from "react-testing-library"
 import Display from "./Display"
+import Controls from "../controls/Controls"
+import Dashboard from "../dashboard/Dashboard"
 
 describe("<Display/>",()=>{
     it("should display unlocked and Open without buttons pressed" ,()=>{
@@ -8,5 +10,14 @@ describe("<Display/>",()=>{
 
         display_component.getByText(/unlocked/i)
         display_component.getByText(/open/i)
+    })
+    it("should display closed when close gate button is pressed ",()=>{
+        const display_component = render(<Display />)
+        const dash_component = render(<Dashboard/>)
+
+        const button = display_component.getByText(/close gate/i)
+
+        fireEvent.click(button)
+        dash_component.getByText(/closed/i)
     })
 })
